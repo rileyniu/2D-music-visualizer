eb562e5907e8d1de4af74884ab93d1c3961e4a87
qingyuhe <hecynthia@gmail.com>
2019-12-08 13:37:57 -0500

    Extracts MIDIpitch, frequency, and beat positions

--- essentia_python.py
+++ essentia_python.py
@@ -1,18 +1,19 @@
 # This is how the audio we want to process sounds like
 import IPython
 import numpy
+import json 
 import essentia
 import essentia.standard as es
 from essentia.standard import *
 from pylab import plot, show, figure, imshow
 import matplotlib.pyplot as plt
 
-IPython.display.Audio('../../../essentia/test/audio/flamenco.wav')
+pool = essentia.Pool(); 
 
 # Compute all features, aggregate only 'mean' and 'stdev' statistics for all low-level, rhythm and tonal frame features
 features, features_frames = es.MusicExtractor(lowlevelStats=['mean', 'stdev'],
                                               rhythmStats=['mean', 'stdev'],
-                                              tonalStats=['mean', 'stdev'])('../../../test/audio/flamenco.wav')
+                                              tonalStats=['mean', 'stdev'])('flamenco.wav')
 
 # See all feature names in the pool in a sorted order
 print(sorted(features.descriptorNames()))
@@ -35,68 +36,41 @@ print("Key/scale estimation (using a profile specifically suited for electronic 
 # BPM Detection
 
 # Loading audio file
-audio = MonoLoader(filename='../../../test/audio/flamenco.wav')()
+audio = MonoLoader(filename='flamenco.wav')()
 
-# Compute beat positions and BPM
+# # Compute beat positions and BPM
 rhythm_extractor = RhythmExtractor2013(method="multifeature")
 bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(audio)
 
-print("BPM:", bpm)
-print("Beat positions (sec.):", beats)
-print("Beat estimation confidence:", beats_confidence)
-
-# Mark beat positions on the audio and write it to a file
-# Let's use beeps instead of white noise to mark them, as it's more distinctive
-marker = AudioOnsetsMarker(onsets=beats, type='beep')
-marked_audio = marker(audio)
-MonoWriter(filename='../../../test/audio/flamenco.wav')(marked_audio)
-
-plt.rcParams['figure.figsize'] = (15, 6) # set plot sizes to something larger than default
-
-plot(audio)
-for beat in beats:
-    plt.axvline(x=beat*44100, color='red')
-
-plt.title("Audio waveform and the estimated beat positions")
-
-peak1_bpm, peak1_weight, peak1_spread, peak2_bpm, peak2_weight, peak2_spread, histogram = BpmHistogramDescriptors()(beats_intervals)
-
-print("Overall BPM (estimated before): %0.1f" % bpm)
-print("First histogram peak: %0.1f bpm" % peak1_bpm)
-print("Second histogram peak: %0.1f bpm" % peak2_bpm)
-
-fig, ax = plt.subplots()
-ax.bar(range(len(histogram)), histogram, width=1)
-ax.set_xlabel('BPM')
-ax.set_ylabel('Frequency')
-plt.title("BPM histogram")
-ax.set_xticks([20 * x + 0.5 for x in range(int(len(histogram) / 20))])
-ax.set_xticklabels([str(20 * x) for x in range(int(len(histogram) / 20))])
-plt.show()
+# print("BPM:", bpm)
+# print("Beat positions (sec.):", beats)
+# print("Beat estimation confidence:", beats_confidence)
 
 # Melody Detection 
 
 # Load audio file; it is recommended to apply equal-loudness filter for PredominantPitchMelodia
-loader = EqloudLoader(filename='../../../test/audio/flamenco.wav', sampleRate=44100)
+loader = EqloudLoader(filename='flamenco.wav', sampleRate=44100)
 audio = loader()
-print("Duration of the audio sample [sec]:")
-print(len(audio)/44100.0)
+#print("Duration of the audio sample [sec]:")
+#print(len(audio)/44100.0)
 
-# Extract the pitch curve
-# PitchMelodia takes the entire audio signal as input (no frame-wise processing is required)
-pitch_extractor = PredominantPitchMelodia(frameSize=2048, hopSize=128)
+# frameSize = the frame size for computing pitch salience
+# hop size = the hop size with which the pitch salience function was computed
+pitch_extractor = PredominantPitchMelodia(frameSize=2048, hopSize=1024)
 pitch_values, pitch_confidence = pitch_extractor(audio)
+
+midi_extractor = PitchContourSegmentation(hopSize=1024)
+onset, duration, midi_pitch = midi_extractor(pitch_values, audio)
 
 # Pitch is estimated on frames. Compute frame time positions
 pitch_times = numpy.linspace(0.0,len(audio)/44100.0,len(pitch_values) )
 
-# Plot the estimated pitch contour and confidence over time
-f, axarr = plt.subplots(2, sharex=True)
-axarr[0].plot(pitch_times, pitch_values)
-axarr[0].set_title('estimated pitch [Hz]')
-axarr[1].plot(pitch_times, pitch_confidence)
-axarr[1].set_title('pitch confidence')
-plt.show()
+#Storing in Pool
+pool.add('MIDIduration', duration)
+pool.add('MIDIpitch', midi_pitch)
+pool.add('pitch', pitch_values)
+pool.add('beats', beats)
  
-output = YamlOutput(filename = 'mfcc.sig') # use "format = 'json'" for JSON output
+output = YamlOutput(format = 'json') # use "format = 'json'" for JSON output
+output = YamlOutput(filename = 'output.json')
 output(pool)
